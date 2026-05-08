9ea52b6881cf42de95f43c7d843bfea5af97e550
19Kaan25 <kaan.koeten@gmx.de>
2026-05-08 11:04:14 +0200

    CrossOrigin

--- src/main/java/htw/webtech/studyhub/demo/DemoController.java
+++ src/main/java/htw/webtech/studyhub/demo/DemoController.java
@@ -1,11 +1,13 @@
 package htw.webtech.studyhub.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://studyhub-frontend-f45v.onrender.com"})
 public class DemoController {
     @GetMapping("/demo")
     public List<Demo> demo() {
